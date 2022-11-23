38ccd52c15e6565cdb5503a5018c9b9b1cd2ea18
Douglas Oliveira <doglasolivera13@gmail.com>
2022-11-23 11:01:54 -0300

    Exercicio 1 - declarando constantes e variaveis

---
+++ fundamentos/secao-04-javascript/dia01/exercicio1/script.js
@@ -0,0 +1,7 @@
+const myName = 'Douglas';
+const birthCity = 'Porto Alegre - RS';
+let birthYear = 2002;
+console.log(myName, birthCity, birthYear);
+birthYear = 2030;
+console.log(birthYear);
+birthCity = 'Viamão - RS' // resultado é erro pois constantes não podem ter seu valor reatribuido.
