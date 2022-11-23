0941a94e4c746ea390d9990a5ae3e31c391facef
Douglas Oliveira <doglasolivera13@gmail.com>
2022-11-23 14:59:54 -0300

    exercicio 4: switch/case

---
+++ fundamentos/secao-04-javascript/dia01/exercicio4/script.js
@@ -0,0 +1,18 @@
+let condicao = '';
+
+switch (condicao) {
+    case "aprovado":
+        console.log("Parabéns, você foi aprovado(a).");
+        break;
+    
+    case "lista":
+        console.log("Você está na lista de espera.");
+        break;
+    
+    case "reprovado":
+        console.log("Você foi reprovado(a).");
+        break;
+    
+    default: 
+        console.log("Informação incorreta")
+}
