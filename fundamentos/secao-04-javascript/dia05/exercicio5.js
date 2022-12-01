cd2c66ba6f961002aea00adf4910207adca448f3
Douglas Oliveira <doglasolivera13@gmail.com>
2022-12-01 16:45:39 -0300

    Modifique a estrutura das funções a seguir para que elas sejam arrow functions: // Transforme a função numeroAleatorio em uma arrow function

---
+++ fundamentos/secao-04-javascript/dia05/exercicio5.js
@@ -0,0 +1,4 @@
+// Modifique a estrutura das funções a seguir para que elas sejam arrow functions:
+// Transforme a função numeroAleatorio em uma arrow function
+const numeroAleatorio = () => Math.random();
+  console.log(numeroAleatorio());
