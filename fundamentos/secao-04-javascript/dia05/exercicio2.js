8c920c48382cd9ae892edf769f42247a33bd94c8
Douglas Oliveira <doglasolivera13@gmail.com>
2022-12-01 16:44:38 -0300

    Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const

---
+++ fundamentos/secao-04-javascript/dia05/exercicio2.js
@@ -0,0 +1,9 @@
+// Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const
+const pessoa = {
+    nome: 'Henri',
+    idade: 20
+}
+pessoa.nome = 'Luna'
+pessoa.idade = 19;
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
