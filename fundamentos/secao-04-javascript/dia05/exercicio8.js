082373259bf207d707e05025dc5af1864476b753
Douglas Oliveira <doglasolivera13@gmail.com>
2022-12-01 16:47:22 -0300

    Transforme a função nomeCompleto em uma arrow function

---
+++ fundamentos/secao-04-javascript/dia05/exercicio8.js
@@ -0,0 +1,4 @@
+// Altere a expressão if/else utilizando ternary operator;
+let speed = 90;
+const speedCar = () => speed >= 120 ? 'Você ultrapassou o limite de velocidade' : 'Você esta na velocidade permitida'
+console.log(speedCar(speed));
