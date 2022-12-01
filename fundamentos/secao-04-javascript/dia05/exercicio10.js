29b89a35c9f17f291c64a105996a86614d567b79
Douglas Oliveira <doglasolivera13@gmail.com>
2022-12-01 20:09:21 -0300

    Crie uma função que calcule a área de um círculo.

---
+++ fundamentos/secao-04-javascript/dia05/exercicio10.js
@@ -0,0 +1,6 @@
+// Crie uma função que calcule a área de um círculo.
+
+let pi = 3.14;
+const circleArea = (raio) => console.log(`Essa é a área do círculo: ${(pi * raio) * raio}`) 
+
+circleArea(5);
