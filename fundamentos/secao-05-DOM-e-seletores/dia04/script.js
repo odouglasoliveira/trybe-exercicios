85852cc1e90ef4be3a08ef4813fcd77a0ae76f54
Douglas Oliveira <doglasolivera13@gmail.com>
2022-12-13 15:40:21 -0300

    Exemplo de utilização do localStorage

--- fundamentos/secao-05-DOM-e-seletores/dia04/script.js
+++ fundamentos/secao-05-DOM-e-seletores/dia04/script.js
@@ -1,9 +1,38 @@
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
-sessionStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
-sessionStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
-console.log(sessionStorage.getItem('lastname')); // retorna o valor 'Smith'
-console.log(sessionStorage.length); // possui duas entradas, então o retorno é o valor: 2
-sessionStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
-console.log(sessionStorage.length); // possui uma entrada, então o retorno é o valor: 1
-sessionStorage.clear(); // limpa todas as entradas salvas em sessionStorage
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+const insertPhraseInDOM = () => {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
+
+const addPhraseToLocalStorage = () => {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+}
+
+const initialRendering = () => {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    }
+  }
+}
+
+button.addEventListener('click', addPhraseToLocalStorage);
+
+window.onload = () => initialRendering();
